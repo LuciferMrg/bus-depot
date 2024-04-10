bdf0f10f1e7a293fb094f23dd09d7025d58d2411
Yevhenii Skuratov <gastor.lastor@gmail.com>
2024-04-11 00:18:24 +0300

    feat: add bus and station route for express use

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const cookieParser = require('cookie-parser');
 
 const userRoute = require('./routes/user-route');
 const driverRoute = require('./routes/driver-route');
+const busRoute = require('./routes/bus-route');
+const stationRoute = require('./routes/station-route');
 
 const server = express();
 
@@ -12,6 +14,8 @@ server.use(express.urlencoded({extended: true}));
 
 server.use('/api', userRoute);
 server.use('/api', driverRoute);
+server.use('/api', busRoute);
+server.use('/api', stationRoute);
 
 
 module.exports = server;
