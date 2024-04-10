fb50600f07601a7e4a4d9aa04de5eb2a5ba9bd22
Yevhenii Skuratov <gastor.lastor@gmail.com>
2024-04-10 22:53:10 +0300

    change: change route path for "driver-route"

--- routes/driver-route.js
+++ routes/driver-route.js
@@ -10,45 +10,39 @@ const {STATUS_CODES, sendError, sendResult, ROLES} = require('../utils/utilities
 
 const router = express.Router();
 
-router.post('/driver/add', validationMiddleware.driverValidation, validationMiddleware.isValid, (req, res, next) => {
-    const firstName = req.body.firstName;
-    const lastName = req.body.lastName;
-    const address = req.body.address;
-    const phoneNumber = req.body.phoneNumber;
+router.get('/drivers', (req, res, next) => {
+    DriverController.getAllDrivers()
+        .then((result) => sendResult(res, next, STATUS_CODES.SUCCESS, result))
+        .catch((error) => sendError(res, next, error.status || STATUS_CODES.INTERNAL_SERVER_ERROR, error));
+});
+
+router.get('/drivers/:driverId', (req, res, next) => {
+    const driverId = req.params.driverId;
+
+    DriverController.getDriver(driverId)
+        .then((result) => sendResult(res, next, STATUS_CODES.SUCCESS, result))
+        .catch((error) => sendError(res, next, error.status || STATUS_CODES.INTERNAL_SERVER_ERROR, error));
+});
+
+router.post('/drivers', validationMiddleware.driverValidation, validationMiddleware.isValid, (req, res, next) => {
+    const {firstName, lastName, address, phoneNumber} = req.body;
 
     DriverController.addDriver(firstName, lastName, address, phoneNumber)
         .then((result) => sendResult(res, next, STATUS_CODES.SUCCESS, result))
         .catch((error) => sendError(res, next, error.status || STATUS_CODES.INTERNAL_SERVER_ERROR, error));
 });
 
-router.get('/driver/getAll', (req, res, next) => {
-    DriverController.getAllDrivers()
-       .then((result) => sendResult(res, next, STATUS_CODES.SUCCESS, result))
-       .catch((error) => sendError(res, next, error.status || STATUS_CODES.INTERNAL_SERVER_ERROR, error));
-});
-
-router.post('/driver/getDriver', (req, res, next) => {
-    const driverId = req.body.driverId;
-
-    DriverController.getDriver(driverId)
-       .then((result) => sendResult(res, next, STATUS_CODES.SUCCESS, result))
-       .catch((error) => sendError(res, next, error.status || STATUS_CODES.INTERNAL_SERVER_ERROR, error));
-});
-
-router.put('/driver/update', validationMiddleware.driverValidation, validationMiddleware.isValid, (req, res, next) => {
-    const driverId = req.body.driverId;
-    const firstName = req.body.firstName;
-    const lastName = req.body.lastName;
-    const address = req.body.address;
-    const phoneNumber = req.body.phoneNumber;
+router.put('/drivers/:driverId', validationMiddleware.driverValidation, validationMiddleware.isValid, (req, res, next) => {
+    const driverId = req.params.driverId;
+    const {firstName, lastName, address, phoneNumber} = req.body;
 
     DriverController.updateDriver(driverId, firstName, lastName, address, phoneNumber)
-       .then((result) => sendResult(res, next, STATUS_CODES.SUCCESS, result))
-       .catch((error) => sendError(res, next, error.status || STATUS_CODES.INTERNAL_SERVER_ERROR, error));
+        .then((result) => sendResult(res, next, STATUS_CODES.SUCCESS, result))
+        .catch((error) => sendError(res, next, error.status || STATUS_CODES.INTERNAL_SERVER_ERROR, error));
 });
 
-router.delete('/driver/delete', (req, res, next) => {
-    const driverId = req.body.driverId;
+router.delete('/drivers/:driverId', (req, res, next) => {
+    const driverId = req.params.driverId;
 
     DriverController.deleteDriver(driverId)
         .then((result) => sendResult(res, next, STATUS_CODES.SUCCESS, result))
