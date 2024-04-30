47cf9a5902246c0613d2f0b4f33f948b7951488a
Yevhenii Skuratov <gastor.lastor@gmail.com>
2024-04-30 16:34:43 +0300

    change: change database config

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 const connectDatabase = () => {
-    mongoose.connect(process.env.DB_URI,).then(con => console.log(`MongoDB Database connected with HOST: ${con.connection.host}`))
+    mongoose.connect(process.env.DB_URI,{dbName: 'test'}).then(con => console.log(`MongoDB Database connected with HOST: ${con.connection.host}`))
         .catch((err) => console.log(`MongoDB Database connection error: \n${err}`));
 }
 
